ad6a42646b52a22f8d53d35e6e30f387c16a8704
mathmakgakpak <44949126+mathmakgakpak@users.noreply.github.com>
2020-01-19 09:49:23 +0100

    anti pong for users

--- scripts/discord.js
+++ scripts/discord.js
@@ -1,10 +1,11 @@
+//module by mathias377
 module.exports = (() => {
   const Discord = require("discord.js");
   let name = "Discord Gateway"
-  let version = "1.0.0"
+  let version = "1.0.4"
 
   function install() {
-    let config = new server.ConfigManager(name, { //shitty
+    let config = new server.ConfigManager(name, {
       guildId: "",
       channelId: { //channelId: world
         "": "main"
@@ -32,7 +33,6 @@ module.exports = (() => {
         if (message.author.bot) return;
         if (message.channel.type === "dm") return;
 
-        //if (configg.enablePings == false) message.content = message.content.replace(/<@([0-9]+)>/g, "(here ping)");
         let world = server.worlds.find(function(world) {
           return world.name == config.channelId[message.channel.id]
         });
@@ -44,6 +44,7 @@ module.exports = (() => {
     server.events.on("chat", function(client, msg) {
       var channelId = getKeyByValue(config.channelId, client.world)
       if (channelId == false) return;
+      if (client.rank != 3) msg = msg.replace(/<@.!?([0-9]+)>/g, "(here ping)");
       let before = client.before.replace(/<(?:alt=("|')(.+?)\1|.|\n)+>/gm, "$2");
       bot.guilds.get(config.guildId).channels.get(channelId).send(`${before}: ${msg}`)
     })
